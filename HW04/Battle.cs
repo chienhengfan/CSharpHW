843fda5fc5851919b26967654ec4038bc955187c
chienheng <harry1995841025@gmail.com>
2023-05-16 21:41:55 +0800

    HW04

---
+++ HW04/Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW04
+{
+    class Battle
+    {
+        public void AttackEnemy(Character Offender,Character Defender)
+        {
+            Defender.HP -= Offender.Attack; 
+        }
+    }
+}
---
+++ HW04/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW04
+{
+    class Character
+    {
+        private float _hp;
+        public float HP { private set { value = _hp; } get { return _hp; } }
+        private float _attack;
+        public float Attack { private set { value = _attack; } get { return _attack; } }
+        private float _agile;
+        public float Agile { private set { value = _attack; } get { return _attack; } }
+        public enum statchacter { Health, Dying, Dead }
+        public statchacter inintialStat;
+
+        public void ShowStatTable()
+        {
+            Console.WriteLine($"STAT: {inintialStat}\nHP:{HP}");
+        }
+        
+    }
+}
